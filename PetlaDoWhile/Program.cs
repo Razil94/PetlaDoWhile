359b893432b9d9495bd2d6022a7d9509d3bb5961
Lukas <razilgry@gmail.com>
2023-12-09 06:51:28 +0100

    Dodaj pliki projektów.

---
+++ PetlaDoWhile/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int liczba = 1;
+        do 
+                {
+            Console.WriteLine(Math.Pow(liczba, 2));
+
+        }
+        while ( liczba ++ < 20) ;
+    }
+}
